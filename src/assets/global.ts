70543923603a20410e7e1da98a1cd82c8a510ac5
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-11-20 00:52:24 -0300

    Update global.ts

--- src/assets/global.ts
+++ src/assets/global.ts
@@ -1,37 +1,38 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { css, createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-${css`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-      "Helvetica Neue", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: #1f1f1f !important;
+const styled = require("styled-components");
 
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
+const GlobalStyle = styled.createGlobalStyle`
+  ${styled.css`
+    body {
       margin: 0;
-    }
-  }
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+        "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      background-color: #1f1f1f !important;
 
-  button {
-    border: none;
-    background-color: transparent;
-    line-height: normal;
-
-    &:focus {
-      outline: none !important;
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        margin: 0;
+      }
     }
-  }
-`}
+
+    button {
+      border: none;
+      background-color: transparent;
+      line-height: normal;
+
+      &:focus {
+        outline: none !important;
+      }
+    }
+  `}
 `;
 
 export default GlobalStyle;
