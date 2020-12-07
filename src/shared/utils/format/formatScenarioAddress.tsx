ae0553934ce3a43d5ca89fdfb7d83bfcde6dcee9
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-12-07 17:36:09 -0300

    Alterando função de formatar endereço do cenario para retornar o endereço completo e o JSX

--- src/shared/utils/format/formatScenarioAddress.tsx
+++ src/shared/utils/format/formatScenarioAddress.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import { RiskLocation } from "types/Plan";
 
 export default function formatScenarioAddress(location: RiskLocation) {
-  return (
-    <>
-      {location.identification},
-      <br />
-      {location.street}, {location.neighbor}
-      <br />
-      {location.complement ? `${location.complement}, ` : ""}
-      {location.city}, {location.state}
-    </>
-  );
+  const address = {
+    fullAddress: `${location.identification},\n`,
+    jsxElement: (
+      <>
+        {location.identification},
+        <br />
+        {location.street}, {location.neighbor}
+        <br />
+        {location.complement ? `${location.complement}, ` : ""}
+        {location.city}, {location.state}
+      </>
+    ),
+  };
+
+  address.fullAddress += `${location.street}, ${location.neighbor}\n`;
+
+  if (location.complement) {
+    address.fullAddress += `${location.complement}, `;
+  }
+
+  address.fullAddress += `${location.city}, ${location.state}`;
+
+  return address;
 }
