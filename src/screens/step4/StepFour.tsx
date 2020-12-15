3923472bd66aed41518294cea3309f91fc4c7e89
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-12-15 13:21:56 -0300

    Escondendo json e checkbox de salvar dados

--- src/screens/step4/StepFour.tsx
+++ src/screens/step4/StepFour.tsx
@@ -300,13 +300,13 @@ const StepFour: React.FC = () => {
         </Button>
       </Container>
 
-      <Form.Check
+      {/* <Form.Check
         label="Salvar cenários"
         checked={scenarioSaveEnabled}
         onChange={() => setScenarioSaveEnabled((oldValue) => !oldValue)}
-      />
+      /> */}
 
-      <div style={{ marginTop: 48 }}>
+      {/* <div style={{ marginTop: 48 }}>
         <div>
           Linhas adicionadas: {scenariosList.length}
           <br />
@@ -342,7 +342,7 @@ const StepFour: React.FC = () => {
             </code>
           ))}
         </div>
-      </div>
+      </div> */}
 
       <LocationModal show={showLocationModal} setShow={setShowLocationModal} />
 
