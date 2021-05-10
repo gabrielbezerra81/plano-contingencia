4899c453431d1dc2465184ecd04ee4b6fa7c4c51
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2021-05-10 18:31:29 -0300

    update auth url

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 
 //keycloak init options
 let initOptions = {
-  url: "https://auth.defesacivil.site/auth",
+  url: "https://auth.defesacivil.website/auth",
   realm: "dc_auth",
   clientId: "contingencia_react",
   onLoad: "login-required",
